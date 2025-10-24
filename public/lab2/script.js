e07f57f3b4f1886c3bbdf06903d7ba4b2eb6e87c
aleksandra <sashka_kolm@mail.ru>
2025-10-24 13:52:55 +0300

    added task list with buttons

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -61,19 +61,29 @@ function loadPage() {
         listOfTasks.push({id: listOfTasks.length,
             taskName: taskInput.value,
             deadline: taskDate.value,
-            type: taskType.value
+            type: taskType.value,
+            isDone: false,
+            isFav: false
         });
         console.log(listOfTasks); 
         console.log('button clicked. task created.');
         saveToStorage();
+        renderTasks();
         taskCreation.reset(); //именно поэтому сделана form, а не label
     })
     taskCreation.appendChild(taskCreationButton);
             
     main.appendChild(taskCreation);
-            
+          
     app.appendChild(header);
     app.appendChild(main);
+
+    const container = document.createElement('div');
+    container.id = 'containerForTasks';
+    container.className = 'container-for-tasks';
+    app.appendChild(container);
+
+    renderTasks();
 
 }
 
@@ -83,4 +93,132 @@ document.addEventListener('DOMContentLoaded', loadPage);
 
 function saveToStorage() {
     localStorage.setItem('currentTasks', JSON.stringify(listOfTasks));
+}
+
+function loadFromStorage() {
+    const curTasks = localStorage.getItem('currentTasks');
+    if (curTasks) {
+        listOfTasks = JSON.parse(curTasks);
+    }
+}
+
+function renderTasks() {
+    loadFromStorage();
+
+    const container = document.getElementById('containerForTasks');
+    container.replaceChildren();
+
+    const fragment = document.createDocumentFragment();
+  
+    listOfTasks.forEach(t => {
+        const taskElement = document.createElement('div');
+        taskElement.className = 'task-item';
+        taskElement.draggable = 'true';
+        
+        const title = document.createElement('h3');
+        title.textContent = t.taskName;
+        
+        const infoDiv = document.createElement('div');
+        infoDiv.className = 'task-info';
+        
+        const deadline = document.createElement('span');
+        deadline.textContent = `deadline: ${t.deadline}`;
+        
+        const type = document.createElement('span');
+        type.textContent = `type: ${t.type}`;
+        type.className = 'task-type';
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.textContent = '✖';
+        deleteBtn.className = 'delete-task-btn';
+        deleteBtn.addEventListener('click', () => {
+            deleteTask(t.id);
+        });
+
+        let doneBtn = document.createElement('button');
+        doneBtn.textContent = t.isDone ? '⚑' : '⚐';
+        doneBtn.className = `done-task-btn ${t.isDone ? 'completed' : ''}`;
+
+        doneBtn.addEventListener('click', () => {
+            markTaskAsDone(t.id);
+        });
+
+        let favBtn = document.createElement('button');
+        favBtn.textContent = t.isFav ?'★' : '☆'; //тернарный оператор дял красоты
+        favBtn.className = `favor-task-btn ${t.isFav ? 'fav' : ''}`;
+
+        favBtn.addEventListener('click', () => {
+            favTask(t.id);
+        });
+        
+        infoDiv.appendChild(deadline);
+        infoDiv.appendChild(type);
+
+        taskElement.appendChild(doneBtn);
+        taskElement.appendChild(title);
+        taskElement.appendChild(infoDiv);
+        taskElement.appendChild(favBtn);
+        // taskElement.appendChild(editTask);
+        taskElement.append(deleteBtn);
+        
+        fragment.appendChild(taskElement);
+
+    });
+
+    container.appendChild(fragment);
+
+    const clearButton = document.createElement('button');
+    clearButton.textContent = '❌';
+    clearButton.className = 'clear-container-btn';
+    clearButton.addEventListener('click', () => {
+        clearTaskContainer();
+    });
+    
+    container.appendChild(clearButton);
+}
+
+function clearTaskContainer() {
+    const container = document.getElementById('containerForTasks');
+    container.replaceChildren();
+
+    listOfTasks = [];
+    saveToStorage();
+}
+
+function deleteTask(taskId) {
+    listOfTasks = listOfTasks.filter(t => t.id !== taskId);
+
+    listOfTasks.forEach((t, index) => {
+        t.id = index;
+    });
+    saveToStorage();
+    renderTasks();
+}
+
+function markTaskAsDone(taskId){
+    task = listOfTasks.find(t => t.id == taskId);
+
+if (task) {
+    task.isDone = !task.isDone;
+    saveToStorage();
+    renderTasks();
+    console.log(`Task "${task.taskName}" marked as ${task.isDone ? 'done' : 'not done'}`);
+    };
+
+    saveToStorage();
+    renderTasks();
+}
+
+function favTask(taskId){
+    task = listOfTasks.find(t => t.id == taskId);
+
+if (task) {
+    task.isFav = !task.isFav; // меняет состояние, чтобы кнопка две функции выполняла
+    saveToStorage();
+    renderTasks();
+    console.log(`Task "${task.taskName}" marked as ${task.isFav ? 'fav' : 'not fav'}`);
+    };
+
+    saveToStorage();
+    renderTasks();
 }
